41f52b6c3e1f3d9cb1c394e86ef04e795c714814
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2022-01-27 15:30:50 -0600

    started working on exclusive roles

--- commands/globalrole.js
+++ commands/globalrole.js
@@ -7,21 +7,29 @@ module.exports = {
     .setDescription("Admin: Give everyone in the server a role")
     .addRoleOption((option) =>
       option
-        .setName("role")
-        .setDescription("Role to give to everyone")
+        .setName("role-given")
+        .setDescription("Role to give to required role")
+        .setRequired(true)
+    )
+    .addRoleOption((option) =>
+      option
+        .setName("role-require")
+        .setDescription("Role required to get role")
         .setRequired(true)
     ),
   async execute(interaction) {
     if (interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
       console.log("This member is an admin");
-      const role = interaction.options.getRole("role");
+      const role = interaction.options.getRole("role-given");
 
-      interaction.guild.members.cache
-        .filter((m) => !m.user.bot)
-        .forEach((member) => {
-          console.log(member.user.username);
-          member.roles.add(role);
-        });
+      // interaction.guild.members.cache
+      //   .filter((m) => !m.user.bot)
+      //   .forEach((member) => {
+      //     console.log(member.user.username);
+      //     // member.roles.add(role);
+      //   });
+
+      console.log(interaction.guild);
 
       // interaction.member.roles.add(role);
 
